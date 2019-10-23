168fb50fa4f8cd827670e78fe88cf98c61d4b687
Slava <ucheba00@gmail.com>
2019-10-23 15:29:38 -0600

    add all the missing shapshots

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -3,7 +3,9 @@ import App from './App.jsx';
 import renderer from 'react-test-renderer';
 
 test(`App's snapshot`, () => {
-  const tree = renderer.create(<App />).toJSON();
+  const tree = renderer
+    .create(<App settings={{gameTime: 3, errorCount: 3}} questions={[`0`]} />)
+    .toJSON();
 
   expect(tree).toMatchSnapshot();
 });
